4843552b8e56bc4155798197190ab0a068a09929
ayodiya <devayodiya@gmail.com>
2024-08-20 19:23:24 +0100

    add routes/authroutes login route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,10 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
-import { createUser } from "../controllers/authController";
+import { createUser, loginUser } from "../controllers/authController";
 import validate from "../validators/validate";
 import createUserValidation from "../validators/createUserValidation";
+import loginUserValidation from "../validators/loginUserValidation";
 
 router.post("/create-user", createUserValidation(), validate, createUser);
+router.post("/login", loginUserValidation(), validate, loginUser);
 
 export default router;
