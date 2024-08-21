90bae827b19293135926c896f49c151689e7b4b4
ayodiya <devayodiya@gmail.com>
2024-08-21 09:47:19 +0100

    change controllers/authcontroller rename file

---
+++ src/controllers/authController.ts
@@ -0,0 +1,114 @@
+import bcrypt from "bcryptjs";
+import { Request, Response } from "express";
+
+import User from "../models/User";
+import Token from "../models/Token";
+
+export const createUser = async (req: Request, res: Response) => {
+  const { firstName, lastName, email, phoneNo, password } = req.body;
+
+  try {
+    //check if user exists
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ msg: "User already exists" });
+    }
+
+    //create new user
+    await User.create({
+      firstName,
+      lastName,
+      email,
+      phoneNo,
+      password,
+    });
+
+    res.status(201).json({
+      status: "success",
+      message: `User registered successfully`,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Server error, please try again",
+    });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    //check if user exists
+    const userExists = await User.findOne({ email });
+
+    // if user does not exist return 400
+    if (!userExists) {
+      return res
+        .status(400)
+        .json({ msg: "User with the email does not exists" });
+    }
+
+    //compare password  and log in if true else return error
+    if (await userExists.comparePassword(password)) {
+      const token = await userExists.generateAuthToken();
+
+      //save token to database
+      await Token.create({
+        userId: userExists._id,
+        token,
+      });
+
+      res.status(200).json({
+        status: "success",
+        message: `User logged in  successfully`,
+        token,
+      });
+    } else {
+      return res.status(400).json({ msg: "Password not correct" });
+    }
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Server error, please try again",
+    });
+  }
+};
+
+export const logoutUser = async (req: Request, res: Response) => {
+  if (!req.user || !req.user.email) {
+    return res.status(400).json({ msg: "User is not authenticated" });
+  }
+
+  const { email, token } = req.user;
+
+  try {
+    //check if user exists
+    const userExists = await User.findOne({ email });
+
+    // if user does not exist return 400
+    if (!userExists) {
+      return res.status(400).json({ msg: "User is not logged in" });
+    }
+
+    //check if token exists
+    const tokenExists = await Token.findOne({ token });
+
+    // if token does not exist return 400
+    if (!tokenExists) {
+      return res.status(400).json({ msg: "No login session available" });
+    } else {
+      await Token.findOneAndDelete({ token });
+      res.status(200).json({
+        status: "success",
+        message: "Logout successfully",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Server error, please try again",
+    });
+  }
+};
