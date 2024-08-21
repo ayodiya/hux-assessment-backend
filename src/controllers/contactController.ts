be973076f53a2c0291107c771a6e15d9f4a2fca1
ayodiya <devayodiya@gmail.com>
2024-08-21 10:40:44 +0100

    add controllers/contactcontrollers addcontact controller

---
+++ src/controllers/contactController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+
+import Contact from "../models/Contact";
+
+export const addContact = async (req: Request, res: Response) => {
+  const { firstName, lastName, email, phoneNo } = req.body;
+
+  try {
+    //check if contact already exist
+    const contactExists = await Contact.findOne({
+      firstName,
+      lastName,
+      phoneNo,
+    });
+
+    // if contact exists return error else store contact
+    if (contactExists) {
+      return res.status(400).json({ msg: "The contact already exists" });
+    } else {
+      await Contact.create({
+        firstName,
+        lastName,
+        email,
+        phoneNo,
+      });
+
+      res.status(201).json({
+        status: "success",
+        message: `Contact created successfully`,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Server error, please try again",
+    });
+  }
+};
