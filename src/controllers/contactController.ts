ca9a178ba29efe22a7d8e9c03f676fc93f14fbfe
ayodiya <devayodiya@gmail.com>
2024-08-21 11:37:08 +0100

    add controllers/contactcontrollers singlecontact controller

--- src/controllers/contactController.ts
+++ src/controllers/contactController.ts
@@ -54,3 +54,27 @@ export const allContacts = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const singleContact = async (req: Request, res: Response) => {
+  const { slug } = req.params;
+
+  try {
+    //check if contact exists
+    const contactExists = await Contact.findOne({ slug });
+
+    //if contact does not exist return 400
+    if (!contactExists) {
+      return res.status(400).json({ msg: "The contact does not exists" });
+    } else {
+      return res.status(200).json({
+        status: "success",
+        contactExists,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Server error, please try again",
+    });
+  }
+};
